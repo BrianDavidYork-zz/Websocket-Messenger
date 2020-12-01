0ced730d7e2189714814df2c231a5f79215e4157
Brian York <briandavidyork@gmail.com>
2020-12-01 02:01:26 -0500

    websockets;

--- websocket/websocket.go
+++ websocket/websocket.go
@@ -1,6 +1,7 @@
 package websocket
 
 import (
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gobwas/ws"
 	"github.com/gobwas/ws/wsutil"
 	"github.com/golang/glog"
@@ -8,32 +9,50 @@ import (
 	"net/http"
 )
 
-var connectionTable map[string]*net.Conn
+var connectionTable = make(map[string]*net.Conn)
 
 func Create(res http.ResponseWriter, req *http.Request) {
 	// using github.com/gobwas/ws package and docs
-	glog.Info("Create Websocket")
 	conn, _, _, err := ws.UpgradeHTTP(req, res)
 	if err != nil {
 		glog.Error(err)
 	}
-	// echo chat from docs
-	go func() {
-		defer conn.Close()
 
-		for {
-			_, op, err := wsutil.ReadClientData(conn)
-			if err != nil {
-				glog.Error(err)
-			}
-			err = wsutil.WriteServerMessage(conn, op, []byte("same response all the time!"))
-			if err != nil {
-				glog.Error(err)
-			}
-		}
-	}()
+	// on connect, client sends jwt to authorize
+	token, _, err := wsutil.ReadClientData(conn)
+	if err != nil {
+		glog.Error(err)
+	}
+
+	username, err := checkJwt(string(token))
+	if err != nil || username == "" {
+		conn.Close()
+	} else {
+		// if jwt is authorized
+		// save user and connection to table
+		connectionTable[username] = &conn
+	}
 }
 
-func SendWebsocketMessage() (err error) {
+func SendWebsocketMessage(username string, message interface{}) (err error) {
+	return
+}
+
+func checkJwt(bearerToken string) (username string, err error) {
+	token, err := jwt.Parse(bearerToken, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			glog.Error(err)
+		}
+		return []byte("WaterCooler123"), nil
+	})
+	if err != nil {
+		glog.Error(err)
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		glog.Error(err)
+		return
+	}
+	username = claims["username"].(string)
 	return
 }
